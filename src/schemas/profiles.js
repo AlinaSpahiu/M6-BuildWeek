c82ab2ef26dca3c637c238adbd85f4a4aa3a14a9
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-21 14:39:47 +0200

    Update profiles.js

--- src/schemas/profiles.js
+++ src/schemas/profiles.js
@@ -2,10 +2,57 @@
 
 const mongoose = require("mongoose")
 const { Schema } = require("mongoose")
+const v = require("validator")
 
-// const v = require("validator")
+const Profile = new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    surname:{
+        type: String,
+        required: true,
+     },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true, //this is sanitization
+        validate:{
+         validator: async(value) =>{
+           if(!v.isEmail(value)){
+             throw new Error("Email not valid!")
+           } else {
+             // read in the db if the email is in use
+             const checkEmail = await ProfileModel.findOne({email:value})
+             if(checkEmail){
+               throw new Error("email already in use!")
+             }
+           }
+         }
+    },
+    },
+    bio:{
+        type: String,
+        required: true,
+    },
+    title:{
+        type: Array,
+        required: true,
+    },
+    area:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String
+    },
+    username:{
+        type: String,
+        required: true,
+    }
+})
+Profile.set('timestamps', true);
 
-const Profile = new Schema()
 
-const ProfileModel = mongoose.model("posts", Profile)
+const ProfileModel = mongoose.model("profiles", Profile)
 module.exports = ProfileModel 
